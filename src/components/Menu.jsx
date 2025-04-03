7f3255d47b9770c2efc2d455b66bf8ef8418c620
Harel7897 <harel100komemi@gmail.com>
2025-04-03 16:10:44 +0300

    addea tafrit

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -2,15 +2,54 @@ import React from 'react'
 
 const Menu = () => {
     return (
-        <div style={{ padding: '70px',background:'linear-gradient(90deg, #0f2027, #203a43, #2c5364)' }}>
-          <iframe
-            src="/menu.pdf"
-            title="תפריט PDF"
-            width="100%"
-            height="918px"
-            style={{ border: 'none' }}
-          ></iframe>
+    <div style={{ background: 'linear-gradient(90deg, #0f2027, #203a43, #2c5364)'}}>
+
+     <a href="/menu.pdf" target="_blank" rel="noopener noreferrer">
+  <button style={{
+    marginTop: '40px',
+    fontSize: '16px',
+    border: 'none',
+    borderRadius: '8px',
+    cursor: 'pointer',
+    boxShadow: '0 4px 12px rgba(0,0,0,0.2)'
+  }}>
+    צפייה בקובץ PDF
+  </button>
+</a>
+
+
+        <div style={{ padding: '98px', textAlign: 'center' }}>
+
+          <img
+            src="/Tafritimages/part2.png"
+            alt="תפריט חלק 2"
+            style={{
+              maxWidth: '100%',
+              width: '600px',
+              borderRadius: '12px',
+              boxShadow: '0 4px 12px rgba(0,0,0,0.1)'
+            }}
+          />
+
+
+          <img
+            src="/Tafritimages/part1.png"
+            alt="תפריט חלק 1"
+            style={{
+              maxWidth: '100%',
+              width: '600px',
+              marginBottom: '20px',
+              borderRadius: '12px',
+              boxShadow: '0 4px 12px rgba(0,0,0,0.1)'
+            }}
+          />
+
+</div>
+ 
+
         </div>
+
+
       );
     }
     
